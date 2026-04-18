230e730f144bd81410f54477bf6e9adb837d1ffc
Py1h0n <55352640+Py1h0n@users.noreply.github.com>
2026-04-18 12:16:33 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd backend
 pip install -r ../requirements.txt
-playwright install chromium --with-deps
+playwright install chromium --with-deps || true
 uvicorn main:app --host 0.0.0.0 --port $PORT
